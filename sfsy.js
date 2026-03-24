7a49bb53475b5e3910e609fccc1ddc29e5c65bf3
Tzbfire <962831443@qq.com>
2026-03-24 15:54:16 +0800

    Update sfsy.js

--- sfsy.js
+++ sfsy.js
@@ -2,45 +2,42 @@
  *
  *
 [rewrite_local]
-^https:\/\/mcs-mimp-web\.sf-express\.com\/mcs-mimp\/.* url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/sfsy.js
+^https:\/\/mcs-mimp-web\.sf-express\.com\/mcs-mimp\/integralPlanet\/getCxAdvertiseList url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/sfsy.js
 
 [mitm]
 hostname = mcs-mimp-web.sf-express.com
 *
 *
 */
-
-// 名称：sessionId提取器
-// 功能：提取sessionId并通知
-// 作者：元宝
-// 类型：script-request-header
-
+// 优化版：只有三个值都找到时才发送通知
 (function() {
     var request = $request;
     var headers = request.headers;
-    var sessionId = null;
     
-    // 从请求头中查找sessionId
-    if (headers.Cookie && headers.Cookie.match(/sessionId=([^;]+)/)) {
-        sessionId = headers.Cookie.match(/sessionId=([^;]+)/)[1];
-    } else if (headers.cookie && headers.cookie.match(/sessionId=([^;]+)/)) {
-        sessionId = headers.cookie.match(/sessionId=([^;]+)/)[1];
-    } else {
-        // 检查其他头部字段
-        for (var key in headers) {
-            if (typeof headers[key] === 'string' && headers[key].match(/sessionId=([^;,\s]+)/i)) {
-                var match = headers[key].match(/sessionId=([^;,\s]+)/i);
-                if (match) {
-                    sessionId = match[1];
-                    break;
-                }
-            }
-        }
+    // 提取函数
+    function extractCookieValue(cookieString, keyName) {
+        if (!cookieString) return null;
+        var regex = new RegExp(keyName + '=([^;]+)');
+        var match = cookieString.match(regex);
+        return match ? match[1] : null;
     }
     
-    // 如果找到sessionId，发送简单通知
-    if (sessionId) {
-        $notify("SessionID", "", sessionId);
+    // 获取Cookie字符串
+    var cookieStr = headers.Cookie || headers.cookie || '';
+    
+    // 提取三个参数
+    var sessionId = extractCookieValue(cookieStr, 'sessionId');
+    var loginMobile = extractCookieValue(cookieStr, '_login_mobile_');
+    var loginUserId = extractCookieValue(cookieStr, '_login_user_id_');
+    
+    // 只有三个值都找到时才发送通知
+    if (sessionId && loginMobile && loginUserId) {
+        var notificationBody = 
+            "sessionId: " + sessionId + "\n" +
+            "_login_mobile_: " + loginMobile + "\n" +
+            "_login_user_id_: " + loginUserId;
+        
+        $notify("顺丰参数", "已获取全部参数", notificationBody);
     }
     
     $done({});

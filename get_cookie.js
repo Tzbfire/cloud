2dbd5405bd4d6de40ce18699ce0a7c65fc7d3eee
Tzbfire <962831443@qq.com>
2026-03-25 15:39:04 +0800

    Update get_cookie.js

--- get_cookie.js
+++ get_cookie.js
@@ -3,10 +3,10 @@
  *
 [rewrite_local]
 # 网易云音乐 Cookie 获取（新版接口）
-^https:\/\/music\.163\.com\/weapi\/user\/level url script-request-body https://raw.githubusercontent.com/你的仓库/路径/get_cookie.js
+^https:\/\/music\.163\.com\/weapi\/user\/level url script-request-body https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/get_cookie.js
 
 # 网易云音乐 Cookie 获取（旧版接口，备用）
-^https:\/\/music\.163\.com\/weapi\/user\/get url script-request-header https://raw.githubusercontent.com/你的仓库/路径/get_cookie_old.js
+^https:\/\/music\.163\.com\/weapi\/user\/get url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/get_cookie.js
 
 
 [mitm]

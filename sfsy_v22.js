219c1b1929edca3ee8eff3aba8bae01d1f9853fb
Tzbfire <962831443@qq.com>
2026-03-27 22:02:37 +0800

    Update and rename sfsy_v2.js to sfsy_v22.js

--- sfsy_v22.js
+++ sfsy_v22.js
@@ -2,7 +2,7 @@
  *
  *
 [rewrite_local]
-^https:\/\/mcs-mimp-web\.sf-express\.com\/mcs-mimp\/integralPlanet\/getCxAdvertiseList url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/sfsy.js
+^https:\/\/mcs-mimp-web\.sf-express\.com\/mcs-mimp\/integralPlanet\/getCxAdvertiseList url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/sfsy_v22.js
 
 [mitm]
 hostname = mcs-mimp-web.sf-express.com

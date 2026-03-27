26d59ea4f8de201dee629a72f96826643ddf7669
Tzbfire <962831443@qq.com>
2026-03-27 12:00:30 +0800

    Update rewrite_Accept.js

--- rewrite_Accept.js
+++ rewrite_Accept.js
@@ -3,12 +3,12 @@
 *
 [rewrite_local]
 
-*可莉网站重写
 ^https://kelee\.one/Tool/Loon/.* url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/rewrite_Accept.js
 
+^https://ddgksf2013\.top/scripts/.* url script-request-header https://raw.githubusercontent.com/Tzbfire/cloud/refs/heads/main/rewrite_Accept.js
 
 [mitm]
-hostname = kelee.one
+hostname = kelee.one, ddgksf2013.top
 
 *
 *
@@ -17,6 +17,8 @@ hostname = kelee.one
 
 (function() {
   let headers = $request.headers;
-  headers['User-Agent'] = 'Loon/936 CFNetwork/1404.0.5 Darwin/22.3.0';
+  headers['User-Agent'] = 'Quantumult%20X/1.5.5 (iPhone14,3; iOS 16.3.1)';
+  headers['X-Requested-With'] = 'Quantumult X';
   $done({ headers: headers });
 })();
+

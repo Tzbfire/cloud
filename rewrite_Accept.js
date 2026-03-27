0d2b4b0b312ea190660ef9619d864f80810bdf82
Tzbfire <962831443@qq.com>
2026-03-27 12:31:56 +0800

    Update rewrite_Accept.js

--- rewrite_Accept.js
+++ rewrite_Accept.js
@@ -19,13 +19,26 @@ hostname = kelee.one, ddgksf2013.top
   let headers = $request.headers;
   let url = $request.url;
 
+  // 1. kelee.one
   if (url.indexOf('kelee.one/Tool/Loon/') !== -1) {
     headers['User-Agent'] = 'Loon/936 CFNetwork/1404.0.5 Darwin/22.3.0';
   }
 
+  // 2. ddgksf2013.top
   if (url.indexOf('ddgksf2013.top/scripts/') !== -1) {
     headers['User-Agent'] = 'Quantumult%20X/1.5.5 (iPhone14,3; iOS 16.3.1)';
     headers['X-Requested-With'] = 'Quantumult X';
+  }
+
+  // 3. 新增示例1：只改UA
+  if (url.indexOf('新域名/路径') !== -1) {
+    headers['User-Agent'] = '这里写新UA';
+  }
+
+  // 4. 新增示例2：改UA+其他头
+  if (url.indexOf('另一个域名/路径') !== -1) {
+    headers['User-Agent'] = '新UA';
+    headers['X-Requested-With'] = 'XXX';
   }
 
   $done({ headers });
